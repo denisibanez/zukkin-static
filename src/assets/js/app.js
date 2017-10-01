ab4515bda1ab40929f364c626d55e2f2c02490d5
denis <denis.ibanez@outlook.com>
2017-10-01 18:55:23 -0300

    01/10/2017

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,10 +1,29 @@
 (function($){
 
   console.log('js funcionando');
-  console.log('js funcionando');
 
   // your code here
-  $('.carousel').carousel()
 
+  //scrollbarplugin
+  var scrollBar = {
+    init: function(){
+      this.ScrollBar();
+    },
+    ScrollBar: function(){
+      $(".content").mCustomScrollbar({
+        axis:"x"
+      });
+    }
+  }
+
+  //ready
+  $(document).ready(function(){
+
+  });
+
+  //load
+  $(window).on("load",function(){
+    scrollBar.init();
+  });
 
 })(jQuery);
