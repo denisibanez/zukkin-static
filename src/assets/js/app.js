15dba14f23e9e54d4d5a592f8beb5634970945fc
denis <denis.ibanez@outlook.com>
2017-10-09 23:08:16 -0300

    09102017

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,12 +3,13 @@
   console.log('js funcionando');
 
   // your code here
-
-  //scrollbarplugin
-  var scrollBar = {
+  var myFunc = {
     init: function(){
       this.ScrollBar();
       this.arrowCollapseButton();
+      this.menuMobile();
+      this.getModal();
+      this.choseModal();
     },
     ScrollBar: function(){
       $(".content").mCustomScrollbar({
@@ -16,20 +17,50 @@
       });
     },
     arrowCollapseButton: function(){
-      $('body').on('click', '.collapse-btn', function(){
+      $('.collapse-btn').click( function(){
         $(this).toggleClass('arrow-up');
       });
-    }
+    },
+    menuMobile: function(){
+      //open menu slide
+      $('#pm_menu').pushmenu({ button : "#open" });
+
+      $('.pm_menu-body ul li a').click(function(e){
+        e.preventDefault();
+        $('.content-menu--mobile').addClass('show-app');
+      });
+    },
+    getModal: function(){
+      $.get( '/modais/modal.html' , function( data ) {
+        //console.log(data);
+        $('#modal').html(data);
+      });
+    },
+    choseModal:function(){
+      $("a[data-target*='#myModal']" ).on('click', function(){
+        var data = $(this).attr('data-ref');
+        //alert(data);
+        var modal = $('body').find("div#" + data);
+        modal.addClass('show-app');
+        //console.log(  $('body').find("div#" + data));
+
+        $('.closeModal').click(function(e){
+          e.preventDefault();
+          $(this).parents('div.modal-dialog').removeClass('show-app');
+          //removeClass('show-app');
+        });
+      });
+    },
   }
 
   //ready
   $(document).ready(function(){
-
+    //myFunc.init();
   });
 
   //load
   $(window).on("load",function(){
-    scrollBar.init();
+    myFunc.init();
   });
 
 })(jQuery);
